60baae3ba8b2b5a0308104208b5dbc7b266bd917
Thaler <Thaler@TARDIS>
2020-05-06 16:46:28 -0300

    Fixed R-statistics methods

--- method_rStatistics/functions/Rstats_ratio.m
+++ method_rStatistics/functions/Rstats_ratio.m
@@ -18,7 +18,7 @@ xf(1) = x(1); v(1) = 0; d(1) = 0; R(1) = 0;
 
 for k = 2:length(x)
     xf(k) = lambda1*x(k)+(1-lambda1)*xf(k-1);
-    v(k) = lambda2*(x(k)-xf(k))^2+(1-lambda2)*v(k-1);
+    v(k) = lambda2*(x(k)-xf(k-1))^2+(1-lambda2)*v(k-1);
     d(k) = lambda3*(x(k)-x(k-1))^2+(1-lambda3)*d(k-1);
     R(k) = (2-lambda1)*v(k)/d(k);
 end
