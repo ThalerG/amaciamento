14f70c2a63c93044151a01f1136f5ee732312ecb
Thaler <Thaler@TARDIS>
2020-05-15 23:08:29 -0300

    Added error plot to comparison between methods

--- examples/compare_runinDetect_methods.m
+++ examples/compare_runinDetect_methods.m
@@ -10,20 +10,32 @@ c = clock;
 fsave = [rt 'Ferramentas\Arquivos Gerados\methods_graphCompare_' num2str(c(1)-2000) num2str(c(2),'%02d') num2str(c(3),'%02d') '_' num2str(c(4),'%02d') num2str(c(5),'%02d') '\'];
 mkdir(fsave);
 
-ajuste_namac = 1;
+n_methods = 5; % Number of tested methods
+n_adjust = 2; % Number of different adjustments
 
-if ajuste_namac == 0
-    lr_param = [15,62,0.01,0]; % Linear regression parameters: w,r,s,f
-    doubleAvg_param = [30,1,32,7e-4,0]; % Double average parameters: w1,w2,r,s,f
-    Rstats_param = [0.05,0.2,0.2,0.01,62,0]; % R-statistics (significance) parameters: lambda1,lambda2,lambda3,alpha,r,f
-    RstatsRc_param = [0.32,0.35,0.28,0.9,7,0]; % R-statistics (Rc) parameters: lambda1,lambda2,lambda3,Rc,r,f
-    spacedDif_param = [25, 30, 31, 7e-4, 0]; % Spaced difference parameters: w, n, r, s, f
-else
-    lr_param = [60,87,0.05,0]; % Linear regression parameters: w,r,s,f
-    doubleAvg_param = [20,1,10,3e-4,0]; % Double average parameters: w1,w2,r,s,f
-    Rstats_param = [0.2,0.5,0.5,0.5,7,0]; % R-statistics (significance) parameters: lambda1,lambda2,lambda3,alpha,r,f
-    RstatsRc_param = [0.02,0.5,0.26,1.45,18,0]; % R-statistics (Rc) parameters: lambda1,lambda2,lambda3,Rc,r,f
-    spacedDif_param = [35, 5, 9, 1e-4, 0]; % Spaced difference parameters: w, n, r, s, f
+lr_param = NaN(n_adjust,4); % Linear regression parameters: w,r,s,f
+doubleAvg_param = NaN(n_adjust,5); % Double average parameters: w1,w2,r,s,f
+Rstats_param = NaN(n_adjust,6);  % R-statistics (significance) parameters: lambda1,lambda2,lambda3,alpha,r,f
+RstatsRc_param = NaN(n_adjust,6); % R-statistics (Rc) parameters: lambda1,lambda2,lambda3,Rc,r,f
+spacedDif_param = NaN(n_adjust,5); % Spaced difference parameters: w, n, r, s, f
+
+for k0 = 1:n_adjust
+    switch k0
+        case 1 % Minimizes sum(e²) of all samples
+            lr_param(k0,:) = [15,62,0.01,0]; 
+            doubleAvg_param(k0,:) = [30,1,32,7e-4,0]; 
+            Rstats_param(k0,:) = [0.05,0.2,0.2,0.01,62,0];
+            RstatsRc_param(k0,:) = [0.32,0.35,0.28,0.9,7,0]; 
+            spacedDif_param(k0,:) = [25, 30, 31, 7e-4, 0]; 
+            
+        case 2 % Minimizes sum(e²) only of not run-in samples
+            lr_param(k0,:) = [60,87,0.05,0]; 
+            doubleAvg_param(k0,:) = [20,1,10,3e-4,0];
+            Rstats_param(k0,:) = [0.2,0.5,0.5,0.5,7,0];
+            RstatsRc_param(k0,:) = [0.02,0.5,0.26,1.45,18,0];
+            spacedDif_param(k0,:) = [35, 5, 9, 1e-4, 0];
+            
+    end
 end
 
 sampleStart = 1; % Starting sample
@@ -71,76 +83,172 @@ tEst{5} = [13;
            2];      
       
 path = cell(length(fsm),1);
-tErr = cell(length(fsm),1);
+Err = cell(n_methods,n_adjust,length(fsm));
+
+
 for k1 = 1:length(fsm)
-    tErr{k1} = cell(length(fsm{k1}),1);
+    for k0 = 1:n_adjust
+        for k = 1:n_methods
+            Err{k,k0,k1} = NaN(length(fsm{k1}),1);
+        end
+    end
     path{k1} = strcat(fpr,fsm{k1});
 end
 
-s = zeros(1,5);
-sA = zeros(1,5);
-sN = zeros(1,5);
-n = 0; nA = 0; nN = 0;
+
 
 %% Graphs
 
 fName = 'corrente_RMS.mat';
 
-for k1 = sampleStart:length(path)
-    figure;
-    ha = tightPlots(length(path{k1}), 1, 1100, [447 70], 85, [55 25], [65 30],'pixels');
-    
-    for k2 = 1:length(path{k1})
-        axes(ha(k2));
-        load([path{k1}{k2} fName]);
-        dataF = cRMS.data(cRMS.t>0); t = cRMS.t(cRMS.t>0);
-        [~,taLr] = runin_detect_lr(dataF,t,lr_param(1),lr_param(2),lr_param(3),lr_param(4));
-        [~,taDavg] = runin_detect_doubleavg(dataF,t,doubleAvg_param(1),doubleAvg_param(2),doubleAvg_param(3),doubleAvg_param(4),doubleAvg_param(5));
-        [~,taRs] = runin_detect_Rstats(dataF,t,Rstats_param(1),Rstats_param(2),Rstats_param(3),Rstats_param(4),Rstats_param(5),Rstats_param(6));
-        [~,taRsH] = runin_detect_Rstats_hRc(dataF,t,RstatsRc_param(1),RstatsRc_param(2),RstatsRc_param(3),RstatsRc_param(4),RstatsRc_param(5),RstatsRc_param(6));
-        [~,taSdif] = runin_detect_spacedDif(dataF,t,spacedDif_param(1),spacedDif_param(2),spacedDif_param(3),spacedDif_param(4),spacedDif_param(5));
-        dataF = cRMS.data(cRMS.t>0);
-        tErr{k1}{k2} = ([taLr, taRs, taRsH, taDavg, taSdif] - tEst{k1}(k2)).^2;
+for k0 = 1:n_adjust
+    for k1 = sampleStart:length(path)
+        figure;
+        ha = tightPlots(length(path{k1}), 1, 1100, [447 70], 85, [55 25], [65 30],'pixels');
 
-        s = s+tErr{k1}{k2};
-        if k2 == 1
-            sN = sN + tErr{k1}{k2};
-            nN = nN + 1;
-        else
-            sA = sA + tErr{k1}{k2};
-            nA = nA + 1;
+        for k2 = 1:length(path{k1})
+            axes(ha(k2));
+            load([path{k1}{k2} fName]);
+            dataF = cRMS.data(cRMS.t>0); t = cRMS.t(cRMS.t>0);
+            [~,taLr] = runin_detect_lr(dataF,t,lr_param(k0,1),lr_param(k0,2),lr_param(k0,3),lr_param(k0,4));
+            [~,taDavg] = runin_detect_doubleavg(dataF,t,doubleAvg_param(k0,1),doubleAvg_param(k0,2),doubleAvg_param(k0,3),doubleAvg_param(k0,4),doubleAvg_param(k0,5));
+            [~,taRs] = runin_detect_Rstats(dataF,t,Rstats_param(k0,1),Rstats_param(k0,2),Rstats_param(k0,3),Rstats_param(k0,4),Rstats_param(k0,5),Rstats_param(k0,6));
+            [~,taRsH] = runin_detect_Rstats_hRc(dataF,t,RstatsRc_param(k0,1),RstatsRc_param(k0,2),RstatsRc_param(k0,3),RstatsRc_param(k0,4),RstatsRc_param(k0,5),RstatsRc_param(k0,6));
+            [~,taSdif] = runin_detect_spacedDif(dataF,t,spacedDif_param(k0,1),spacedDif_param(k0,2),spacedDif_param(k0,3),spacedDif_param(k0,4),spacedDif_param(k0,5));
+            dataF = cRMS.data(cRMS.t>0);
+            
+            temp = [taLr, taRs, taRsH, taDavg, taSdif] - tEst{k1}(k2);
+            Err{1,k0,k1}(k2) = temp(1);
+            Err{2,k0,k1}(k2) = temp(2);
+            Err{3,k0,k1}(k2) = temp(3);
+            Err{4,k0,k1}(k2) = temp(4);
+            Err{5,k0,k1}(k2) = temp(5);
+                   
+
+            hold on; plot(t,dataF,'LineWidth',1,'color','k'); yl = ylim();
+            c = lines(5);
+            line([tEst{k1}(k2),tEst{k1}(k2)],[0 10],'LineWidth',1,'color','k','LineStyle',':');
+            line([taLr,taLr],[0 10],'LineWidth',1.5,'LineStyle','--','color',c(1,:));
+            line([taRs,taRs],[0 10],'LineWidth',1.5,'LineStyle','--','color',c(2,:));
+            line([taRsH,taRsH],[0 10],'LineWidth',1.5,'LineStyle','--','color',c(3,:));
+            line([taDavg,taDavg],[0 10],'LineWidth',1.5,'LineStyle','--','color',c(4,:));
+            line([taSdif,taSdif],[0 10],'LineWidth',1.5,'LineStyle','--','color',c(5,:));hold off;
+            ylim(yl);
+
+            legend({'Corrente RMS','Instante sugerido','Detecção - Regressão Linear','Detecção - R-statistics (\alpha)','Detecção - R-statistics (Rc)','Detecção - Média dupla', 'Detecção - Diferença espaçada'},'location','eastoutside');
+            xlabel('Tempo [h]'); ylabel('Corrente [A]');
+            set(gca,'FontSize',14); set(gca,'FontName','Times New Roman');
+            yticklabels('auto'); xticklabels('auto')
+            xticks(0:2:20); xlim([0 20]);
+            ylim([1 1.15])
+            if k2 == 1
+                title('Não Amaciado');
+            else
+                title(['Amaciado ' num2str(k2-1)]);
+            end
+            hold off;
         end
-        
-        hold on; plot(t,dataF,'LineWidth',1,'color','k'); yl = ylim();
-        c = lines(5);
-        line([tEst{k1}(k2),tEst{k1}(k2)],[0 10],'LineWidth',1,'color','k','LineStyle',':');
-        line([taLr,taLr],[0 10],'LineWidth',1.5,'LineStyle','--','color',c(1,:));
-        line([taRs,taRs],[0 10],'LineWidth',1.5,'LineStyle','--','color',c(2,:));
-        line([taRsH,taRsH],[0 10],'LineWidth',1.5,'LineStyle','--','color',c(3,:));
-        line([taDavg,taDavg],[0 10],'LineWidth',1.5,'LineStyle','--','color',c(4,:));
-        line([taSdif,taSdif],[0 10],'LineWidth',1.5,'LineStyle','--','color',c(5,:));hold off;
-        ylim(yl);
-        
-        legend({'Corrente RMS','Instante sugerido','Detecção - Regressão Linear','Detecção - R-statistics (\alpha)','Detecção - R-statistics (Rc)','Detecção - Média dupla', 'Detecção - Diferença espaçada'},'location','eastoutside');
-        xlabel('Tempo [h]'); ylabel('Corrente [A]');
-        set(gca,'FontSize',14); set(gca,'FontName','Times New Roman');
-        yticklabels('auto'); xticklabels('auto')
-        xticks(0:2:20); xlim([0 20]);
-        ylim([1 1.15])
-        if k2 == 1
-            title('Não Amaciado');
-        else
-            title(['Amaciado ' num2str(k2-1)]);
-        end
-        hold off;
-        n=n+1;
+        savefig([fsave 'am' num2str(k1) 'Amac_A' num2str(k0) '.fig']);
+        export_fig([fsave 'am' num2str(k1) 'Amac_A' num2str(k0)],'-pdf','-transparent');
+        close
+        clear dataF cRMS
     end
-    savefig([fsave 'am' num2str(k1) 'Amac.fig']);
-    export_fig([fsave 'am' num2str(k1) 'Amac'],'-pdf','-transparent');
-%    close
-    clear dataF cRMS
 end
 
-MSE = s/n;
-MSE_N = sN/nN;
-MSE_A = sA/nA;
+%% Error mean/std
+
+ErrT = cell(n_methods,n_adjust);
+ErrA = cell(n_methods,n_adjust);
+ErrN = cell(n_methods,n_adjust);
+
+SumErrT = NaN(n_methods,n_adjust);
+SumErrN = NaN(n_methods,n_adjust);
+SumErrA = NaN(n_methods,n_adjust);
+
+AvgErrT = NaN(n_methods,n_adjust);
+AvgErrN = NaN(n_methods,n_adjust);
+AvgErrA = NaN(n_methods,n_adjust);
+
+StdErrT = NaN(n_methods,n_adjust);
+StdErrN = NaN(n_methods,n_adjust);
+StdErrA = NaN(n_methods,n_adjust);
+
+for k0 = 1:n_adjust
+    for k = 1:n_methods
+        ErrT{k,k0} = vertcat(Err{k,k0,:});
+        for k1 = 1:length(fsm)
+            ErrN{k,k0} = [ErrN{k,k0};Err{k,k0,k1}(1)];
+            if length(fsm{k1})>1
+                ErrA{k,k0} = [ErrA{k,k0}; vertcat(Err{k,k0,k1}(2:end))];
+            end
+        end
+        SumErrT(k,k0) = sum(abs(ErrT{k,k0}));
+        SumErrN(k,k0) = sum(abs(ErrN{k,k0}));
+        SumErrA(k,k0) = sum(abs(ErrA{k,k0}));
+        
+        AvgErrT(k,k0) = mean(ErrT{k,k0});
+        AvgErrN(k,k0) = mean(ErrN{k,k0});
+        AvgErrA(k,k0) = mean(ErrA{k,k0});
+        
+        StdErrT(k,k0) = std(ErrT{k,k0});
+        StdErrN(k,k0) = std(ErrN{k,k0});
+        StdErrA(k,k0) = std(ErrA{k,k0});
+    end
+end
+
+% All tests
+xi = 0.5:1/(n_adjust+1):1.5; x = reshape(xi(2:end-1)'+(0:(n_methods-1)),[],1);
+y = reshape(AvgErrT',1,[]); e = reshape(StdErrT',1,[]);
+fig = figure;
+fig.Position = 1e3*[0.4110    0.3786    1.1000    2*0.225108910891089];
+for k = 1:n_adjust
+    n = k:n_adjust:length(x);
+    errorbar(x(n),y(n),e(n),'o','LineWidth',1.5);
+    hold on
+end
+xticks(1:n_methods);
+xticklabels({'M1','M2.1','M2.2','M3','M4'})
+legend({'A1','A2'},'location','best'); xlim([0.5 n_methods+0.5]);
+set(gca,'FontSize',14); set(gca,'FontName','Times New Roman');
+ylabel('Corrente [A]');  expandaxes(fig); grid on;
+savefig([fsave 'AvgErrT.fig']);
+export_fig([fsave 'AvgErrT'],'-pdf','-transparent');
+close
+
+% Run-in tests
+xi = 0.5:1/(n_adjust+1):1.5; x = reshape(xi(2:end-1)'+(0:(n_methods-1)),[],1);
+y = reshape(AvgErrN',1,[]); e = reshape(StdErrN',1,[]);
+fig = figure;
+fig.Position = 1e3*[0.4110    0.3786    1.1000    2*0.225108910891089];
+for k = 1:n_adjust
+    n = k:n_adjust:length(x);
+    errorbar(x(n),y(n),e(n),'o','LineWidth',1.5);
+    hold on
+end
+xticks(1:n_methods);
+xticklabels({'M1','M2.1','M2.2','M3','M4'})
+legend({'A1','A2'},'location','best'); xlim([0.5 n_methods+0.5]);
+set(gca,'FontSize',14); set(gca,'FontName','Times New Roman');
+ylabel('Corrente [A]');  expandaxes(fig); grid on;
+savefig([fsave 'AvgErrN.fig']);
+export_fig([fsave 'AvgErrN'],'-pdf','-transparent');
+close
+
+% Already run-in tests
+xi = 0.5:1/(n_adjust+1):1.5; x = reshape(xi(2:end-1)'+(0:(n_methods-1)),[],1);
+y = reshape(AvgErrA',1,[]); e = reshape(StdErrA',1,[]);
+fig = figure;
+fig.Position = 1e3*[0.4110    0.3786    1.1000    2*0.225108910891089];
+for k = 1:n_adjust
+    n = k:n_adjust:length(x);
+    errorbar(x(n),y(n),e(n),'o','LineWidth',1.5);
+    hold on
+end
+xticks(1:n_methods);
+xticklabels({'M1','M2.1','M2.2','M3','M4'})
+legend({'A1','A2'},'location','best'); xlim([0.5 n_methods+0.5]);
+set(gca,'FontSize',14); set(gca,'FontName','Times New Roman');
+ylabel('Corrente [A]');  expandaxes(fig); grid on;
+savefig([fsave 'AvgErrA.fig']);
+export_fig([fsave 'AvgErrA'],'-pdf','-transparent');
+close
