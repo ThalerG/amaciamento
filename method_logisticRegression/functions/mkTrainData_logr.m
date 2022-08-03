59ec8749c40fa472ea0c078ebcc52baf3e9d3e92
ThalerG <thalergabriel@gmail.com>
2022-08-03 17:18:18 -0300

    Fix data selection for sliding window

--- method_logisticRegression/functions/mkTrainData_logr.m
+++ method_logisticRegression/functions/mkTrainData_logr.m
@@ -1,8 +1,8 @@
 function [data,t] = mkTrainData_logr(dataInit, tInit, N, M, D, tEst, minT)
 
 t = tInit(tInit>0);
-dataInit = dataInit(tInit>0);
-
+dataCut = dataInit(tInit>0);
+dataCut = dataCut(t<=minT);
 
 t = t(((N-1)*D+1):end);
 % if tEst > minT
@@ -10,15 +10,14 @@ t = t(((N-1)*D+1):end);
 %     t = t(1:2*L);
 % else
     t = t(t<=minT);
-    dataInit = dataInit(tInit<=minT);
 % end
 
-dataInit = movmean(dataInit,[M-1,0]);
+dataCut = movmean(dataCut,[M-1,0]);
 
-data = nan(length(dataInit)-(N-1)*D,N);
+data = nan(length(dataCut)-(N-1)*D,N);
 
 for n = 1:N
-    data(:,n) = dataInit(((N-n)*D+1):(end-(n-1)*D));
+    data(:,n) = dataCut(((N-n)*D+1):(end-(n-1)*D));
 end
 
 data = data(1:length(t),:);
